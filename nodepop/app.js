1e22e629bb7e52c565f5ab9e37cd371695a0ed9c
Juan Marcos <juanmarcosmarinperez@gmail.com>
2019-02-14 20:51:03 +0100

    01a: En el mismo paso que el 1, pero con cambios muy leves, solo tests.

--- nodepop/app.js
+++ nodepop/app.js
@@ -4,24 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');  // cambio a html en motor de vista
+app.set('view engine', 'html');  // Nota: cambio a html en motor de vista
 app.engine('html', require('ejs').__express);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // CAMBIAR esta linea y ponerla antes de los errores
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+/**
+ * Rutas de nuestra aplicación web
+ */
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
